d22af529e15dd6ecfe92efed3194fc4d28ce1483
Nathan Thorpe <nathan.thorpe@comcast.net>
2012-01-19 14:19:40 -0800

    added the player listeners, still some errors

--- src/me/nathan/techstorm/Techstorm.java
+++ src/me/nathan/techstorm/Techstorm.java
@@ -3,16 +3,18 @@ package me.nathan.techstorm;
 import java.util.logging.Logger;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Event;
 
 public class Techstorm extends JavaPlugin {
 	
 	Logger log = Logger.getLogger("Minecraft");
-	private final techstormPlayerListener playerListener = new techstormPlayerListener(this);
+	private final techstormPlayerListener playerListener = new techstormPlayerListener();
 	
 	public void onEnable(){
 		log.info("Techstorm has been enabled!");
 		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvent(Event.Type., playerListener, Event.Priority.Normal, this);
+		pm.registerEvent(Event.Type.PLAYER_FISH, playerListener, Event.Priority.Normal, this);
+		pm.registerEvent(Event.Type.ENTITY_DEATH, playerListener, Event.Priority.Normal, this);
 	}
 	
 	public void onDisable(){
--- src/me/nathan/techstorm/techstormPlayerListener.java
+++ src/me/nathan/techstorm/techstormPlayerListener.java
@@ -1,9 +1,27 @@
 package me.nathan.techstorm;
 
+import org.bukkit.Server;
+import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerListener;
 
 public class techstormPlayerListener {
-	
-	public class BasicPlayerListener extends PlayerListener{}
+	public void onPlayerFish(PlayerFishEvent event) {
+		Player player = event.getPlayer();
+		Server server = player.getServer();
+		String playername = player.getName();
+		server.broadcastMessage(playername + "has gone fishin");
+	}
+	public void onEntityDeath(EntityDeathEvent event) {
+		Player player = monster.getKiller();
+		Monster monster = event.getEntityId();
+		Server server = player.getServer();
+		String playername = player.getName();
+		String monstername = monster.getEntityId();
+		server.broadcastMessage(playername + "has killed a" + monstername);
+		
+	}
 
 }
